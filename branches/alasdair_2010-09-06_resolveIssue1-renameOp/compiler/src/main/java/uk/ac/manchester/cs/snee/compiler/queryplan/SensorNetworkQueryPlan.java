4cd290ceaa9f92e8609211b45eef0165b94590ce
alasdair.gray <alasdair.gray@43248f56-cd0b-11dd-b6f5-21ad4398c266>
2010-09-06 15:33:56 +0000

    Branch to resolve renaming problem identified as issue 1

---
+++ branches/alasdair_2010-09-06_resolveIssue1-renameOp/compiler/src/main/java/uk/ac/manchester/cs/snee/compiler/queryplan/SensorNetworkQueryPlan.java
@@ -0,0 +1,73 @@
+package uk.ac.manchester.cs.snee.compiler.queryplan;
+
+import org.apache.log4j.Logger;
+
+import uk.ac.manchester.cs.snee.compiler.metadata.schema.SchemaMetadataException;
+import uk.ac.manchester.cs.snee.compiler.metadata.schema.TypeMappingException;
+
+/**
+ * Query Plan for Sensor Network that supports In-Network Query Processing.
+ */
+public class SensorNetworkQueryPlan extends QueryExecutionPlan {
+
+	/**
+	 * Logger for this class.
+	 */
+	private Logger logger = Logger.getLogger(SensorNetworkQueryPlan.class.getName());
+	
+	private DAF daf;
+	
+	private RT rt;
+	
+	private Agenda agenda;
+	
+	/**
+	 * Constructor
+	 * @param dlaf The input DLAF
+	 * @throws TypeMappingException 
+	 * @throws SchemaMetadataException 
+	 */
+	public SensorNetworkQueryPlan(DLAF dlaf, RT rt, DAF daf, Agenda agenda, 
+	String queryName) 
+	throws  SchemaMetadataException, TypeMappingException {
+		super(dlaf, queryName);
+		if (logger.isDebugEnabled())
+			logger.debug("ENTER SensorNetworkQueryPlan()"); 
+		this.rt = rt;
+		this.daf = daf;
+		this.agenda = agenda;
+		if (logger.isDebugEnabled())
+			logger.debug("RETURN SensorNetworkQueryPlan()"); 
+	}
+
+	/**
+	 * @return the daf
+	 */
+	public DAF getDaf() {
+		return daf;
+	}
+
+	/**
+	 * @return the rt
+	 */
+	public RT getRt() {
+		return rt;
+	}
+
+	/**
+	 * @return the agenda
+	 */
+	public Agenda getAgenda() {
+		return agenda;
+	}
+
+	
+//	
+//	protected SensorNetworkQueryPlan(DAF daf, Rt rt. Agenda agenda) {
+//		super(daf.getDLAF());
+//	}
+
+	
+	
+	
+}
